a542cca86a806a016aa86bdaf26e8f537b1353ce
Anmol Kumar <67572237+anmol-kumar0815@users.noreply.github.com>
2026-09-09 21:21:44 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.java
@@ -0,0 +1,29 @@
+class MyHashSet {
+    private List<Integer> data;
+
+    public MyHashSet() {
+        this.data = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        if (!this.data.contains(key)) {
+            this.data.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        this.data.remove(Integer.valueOf(key));
+    }
+    
+    public boolean contains(int key) {
+        return this.data.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
